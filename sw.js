494799c823659b2723a1a592e9604787725e14a7
mohamedstucker <mohamedtucker84@gmail.com>
2026-06-01 18:19:01 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'school-attend-v4';
+const urlsToCache = [
+  '/attendance-working.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+});
